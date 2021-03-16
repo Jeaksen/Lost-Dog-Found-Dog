a8970f9142c9f79879aee68a2f4c31c2d09306d8
Emilia Wróblewska <emiliamwroblewska@gmail.com>
2021-03-16 02:37:14 +0100

    Top bar styling, initial packages

--- Frontend/FrontendApp/src/app/app-routing.module.ts
+++ Frontend/FrontendApp/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterLostDogComponent } from './Components/register-lost-dog/register-lost-dog.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { LoginComponent } from './Components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'register-lost-dog', component: RegisterLostDogComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/FrontendApp/src/app/app.module.ts
+++ Frontend/FrontendApp/src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterLostDogComponent } from './Components/register-lost-dog/register-lost-dog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './Components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterLostDogComponent
+    RegisterLostDogComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
+  ]
 })
 export class AppModule { }
