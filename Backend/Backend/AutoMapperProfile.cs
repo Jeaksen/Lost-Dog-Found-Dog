0e23288f434e8d9ce65c98d3f2c0abb0d605c6ad
Pawel Pohl <pawel.pohl.stud@pw.edu.pl>
2021-03-20 22:58:57 +0100

    Code cleanup

--- Backend/Backend/AutoMapperProfile.cs
+++ Backend/Backend/AutoMapperProfile.cs
@@ -1,14 +1,10 @@
 ﻿using AutoMapper;
-using Backend.DTOs;
 using Backend.DTOs.Authentication;
 using Backend.DTOs.Dogs;
 using Backend.Models.Authentication;
 using Backend.Models.DogBase;
 using Backend.Models.DogBase.LostDog;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Backend
 {
