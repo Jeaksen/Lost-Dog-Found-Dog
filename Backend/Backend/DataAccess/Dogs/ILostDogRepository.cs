ab4c73b5de0d0e79fefe43d83cef9462a827230b
MateuszZalewski0311 <mateusz.zalewski0311@gmail.com>
2021-03-22 13:55:55 +0100

    LostDogDataRepository Implemented

--- Backend/Backend/DataAccess/Dogs/ILostDogRepository.cs
+++ Backend/Backend/DataAccess/Dogs/ILostDogRepository.cs
@@ -11,14 +11,14 @@ namespace Backend.DataAccess.Dogs
     interface ILostDogRepository
     {
         public List<LostDog> GetLostDogs();
-        public LostDog GetUserLostDogs(int ownerId);
+        public List<LostDog> GetUserLostDogs(int ownerId);
         public LostDog AddLostDog(LostDog lostDog);
         public LostDog GetLostDogDetails(int dogId);
         public bool DeleteLostDog(int dogId);
 
         public List<LostDogComment> GetLostDogComments(int dogId);
-        public LostDogComment AddLostDogComment(LostDogComment comment, int dogId);
-        public LostDogComment EditLostDogComment(LostDogComment comment, int dogId);
+        public LostDogComment AddLostDogComment(LostDogComment comment);
+        public LostDogComment EditLostDogComment(LostDogComment comment);
 
     }
 }
--- Backend/Backend/DataAccess/Dogs/LostDogDataRepository.cs
+++ Backend/Backend/DataAccess/Dogs/LostDogDataRepository.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Backend.DataAccess.Dogs
 {
@@ -21,42 +22,109 @@ namespace Backend.DataAccess.Dogs
 
         public LostDog AddLostDog(LostDog lostDog)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var returningDog = _dbContext.LostDogs.Add(lostDog).Entity;
+                _dbContext.SaveChanges();
+                return returningDog;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
         public bool DeleteLostDog(int dogId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var lostDog = _dbContext.LostDogs.Find(dogId);
+                if (lostDog == null) throw new Exception();
+                _dbContext.LostDogs.Remove(lostDog);
+                _dbContext.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public LostDog GetLostDogDetails(int dogId)
         {
-            throw new NotImplementedException();
+            return _dbContext.LostDogs.Find(dogId);
         }
 
         public List<LostDog> GetLostDogs()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _dbContext.LostDogs.OrderBy(ld => ld.Id).ToList();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
-        public LostDog GetUserLostDogs(int ownerId)
+        public List<LostDog> GetUserLostDogs(int ownerId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _dbContext.LostDogs.Where(ld => ld.OwnerId == ownerId).OrderBy(ld => ld.Id).ToList();
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
-        public LostDogComment AddLostDogComment(LostDogComment comment, int dogId)
+        public LostDogComment AddLostDogComment(LostDogComment comment)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var lostDog = _dbContext.LostDogs.Find(comment.DogId);
+                if (lostDog == null) throw new Exception();
+                lostDog.Comments.Add(comment);
+                _dbContext.LostDogs.Update(lostDog);
+                _dbContext.SaveChanges();
+                return comment;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
         public List<LostDogComment> GetLostDogComments(int dogId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                return _dbContext.LostDogs.Find(dogId).Comments;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
 
-        public LostDogComment EditLostDogComment(LostDogComment comment, int dogId)
+        public LostDogComment EditLostDogComment(LostDogComment comment)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var lostDog = _dbContext.LostDogs.Find(comment.DogId);
+                if (lostDog == null) throw new Exception();
+                var oldComment = lostDog.Comments.Find(c => c.Id == comment.Id);
+                if (oldComment == null) throw new Exception();
+                oldComment = comment;
+                _dbContext.LostDogs.Update(lostDog);
+                _dbContext.SaveChanges();
+                return comment;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
     }
 }
