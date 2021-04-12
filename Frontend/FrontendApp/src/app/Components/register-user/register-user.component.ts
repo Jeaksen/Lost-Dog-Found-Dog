612ddb05a773430d9becf61f257962ee323dde15
Mateusz Kubiszewski <63213544+MateuszKubiszewski@users.noreply.github.com>
2021-04-13 01:10:05 +0200

    get dogs for user instead of all dogs in database

--- Frontend/FrontendApp/src/app/Components/register-user/register-user.component.ts
+++ Frontend/FrontendApp/src/app/Components/register-user/register-user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators, ValidationErrors, AbstractControl } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, Validators, ValidationErrors, AbstractControl } from '@angular/forms';
+import { Router } from '@angular/router';
 import { first } from 'rxjs/operators';
 import { RegisterUserRequest } from 'src/app/models/register-user-request';
 import { AuthenticationService } from 'src/app/services/authentication-service';
